2e7631480070e1854f31e82a70f4a426ef967dd5
angel900g <nisarmuskan41@gmail.com>
2025-07-04 17:46:02 +0500

    Fix WebGL Context Creation Errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // Temporarily disable StrictMode to prevent WebGL context creation issues
+  // StrictMode double-invokes effects in development, causing rapid WebGL context creation/destruction
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
